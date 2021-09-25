036d9bc2f8ffcffb98edc7a711c223b70a7779c8
Ricpalo <ricpalo001@gmail.com>
2021-09-25 11:35:54 -0500

    First Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <div>
+            <span onClick={() => window.scroll(0, 0)} className="header">🎬 Entertainment Hub 🎥</span>
+        </div>
+    )
+}
